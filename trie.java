1b2a2b2d411edbf8d64d9e9bbc6399117a6e5d09
Lobna_zrl <lobna.zrelli@gmail.com>
2018-01-30 12:13:53 +0100

    commenter la fonction trie

--- trie.java
+++ trie.java
@@ -6,9 +6,9 @@ int[] list = new int[4];
                 list[1] = 5;
                 list[2] = 596;
                 list[3] = 63;
-
+// sort permet de trier les entier 
                 Arrays.sort(list);
-
+// afficher les resultat sur le console 
                 for(int i = 0; i < list.length; i++)
                         System.out.println(list[i]);
 }
