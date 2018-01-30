3ecb4b267e839bbebd9d2518b5d074e64c19dfe5
Lobna_zrl <lobna.zrelli@gmail.com>
2018-01-30 12:12:05 +0100

    ajout le type de retour de la fonction research

--- search.java
+++ search.java
@@ -1,11 +1,13 @@
 public class Search {
- public static serachWord(String word, String text){
-if(text.contains(word){
-return true;
-}else{
-return false;
-}
-}
+ public static boolean serachWord(String word, String text){
+	if(text.contains(word){
+	return true;
+	}else{
+		return false;
+	}
+ }
+
+
 
 
 }
