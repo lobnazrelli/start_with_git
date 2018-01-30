3248474510259364ad4b5ddceb031fc427a49216
Lobna_zrl <lobna.zrelli@gmail.com>
2018-01-30 11:49:06 +0100

    ajout du class trie

---
+++ trie.java
@@ -0,0 +1,15 @@
+class Trie(){
+
+void trietable(){
+int[] list = new int[4];
+                list[0] =152 ;
+                list[1] = 5;
+                list[2] = 596;
+                list[3] = 63;
+
+                Arrays.sort(list);
+
+                for(int i = 0; i < list.length; i++)
+                        System.out.println(list[i]);
+}
+}
